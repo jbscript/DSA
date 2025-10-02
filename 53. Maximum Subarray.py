5eea21b707c0ad1cf0a7ba1bd820c36cfc57adec
jbscript <jabirjaleel97@gmail.com>
2025-10-02 19:25:03 +0530

    Refactor maxSubArray function to optimize loop structure and remove unnecessary calculations

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,11 +1,27 @@
+# def maxSubArray(nums):
+#     max_sum = float("-inf")
+#     for i in range(len(nums)):
+#         for j in range(len(nums)):
+#             current_sum = 0
+#             for k in range(i, j + 1):
+#                 current_sum += nums[k]
+#             max_sum = max(current_sum, max_sum)
+#     return max_sum
+
+
 def maxSubArray(nums):
     max_sum = float("-inf")
-    for i in range(len(nums)):
-        for j in range(len(nums)):
-            current_sum = 0
-            for k in range(i, j + 1):
-                current_sum += nums[k]
+
+    for i in range(len(nums) - 2):
+        current_sum = nums[i]
+        for j in range(i + 1, len(nums) - 1):
+            print(f"{i} -> {j}")
+            current_sum += nums[j]
             max_sum = max(current_sum, max_sum)
+            # current_sum = 0
+            # for k in range(i, j + 1):
+            # current_sum += nums[k]
+            # max_sum = max(current_sum, max_sum)
     return max_sum
 
 
