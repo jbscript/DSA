b7daa3e36df9bb03905fa5fc35b11c8c99009db0
jbscript <jabirjaleel97@gmail.com>
2025-10-05 20:30:37 +0530

    Add initial implementation of search function for rotated sorted array

---
+++ Array/33. Search in Rotated Sorted Array.py
@@ -0,0 +1,19 @@
+# Example 1:
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+
+# Example 2:
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+
+# Example 3:
+# Input: nums = [1], target = 0
+# Output: -1
+
+
+def search(nums: list[int], target: int) -> int:
+    print()
+
+
+response = search([11, 13, 15, 17])
+print(response)
