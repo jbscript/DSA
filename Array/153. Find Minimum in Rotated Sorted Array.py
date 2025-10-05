1e014734555690f88e2330da10c6825263562826
jbscript <jabirjaleel97@gmail.com>
2025-10-05 20:28:53 +0530

    Refactor findMin function to improve clarity and update test case

--- Array/153. Find Minimum in Rotated Sorted Array.py
+++ Array/153. Find Minimum in Rotated Sorted Array.py
@@ -15,8 +15,16 @@
 
 
 def findMin(nums: list[int]) -> int:
-    print()
+    left = 0
+    right = len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[right]:
+            left = mid + 1
+        else:
+            right = mid
+    return nums[left]
 
 
-response = findMin([3, 4, 5, 1, 2])
+response = findMin([11, 13, 15, 17])
 print(response)
